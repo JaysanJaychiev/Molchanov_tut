861e80f7d9cf90238bd270875f48436ce88d6c72
JaysanJaychiev <jays.26@mail.ru>
2021-02-28 11:08:36 +0600

    apps class

--- blogengine/urls.py
+++ blogengine/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import redirect_blog
 
-from .views import hello
+# from .views import hello
 
 urlpatterns = [
+    path('', redirect_blog),
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls'))
 ]
